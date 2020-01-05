81b6e524e288dc233f968453a9d9b8a61b0565d3
Jun-yeong Jang <joyyir@naver.com>
2020-01-06 00:47:47 +0900

    엔티티 타입 맵핑

--- src/main/java/pe/joyyir/Account.java
+++ src/main/java/pe/joyyir/Account.java
@@ -3,6 +3,9 @@ package pe.joyyir;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.util.Date;
 
 @Entity
 public class Account {
@@ -10,9 +13,13 @@ public class Account {
     @Id @GeneratedValue
     private Long id;
 
+//    @Column // 이것이 생략되어 있는 것임 (unique, nullable 등 설정 가능)
     private String username;
 
     private String password;
+
+    @Temporal(TemporalType.TIMESTAMP) // DATE(날짜), TIME(시간), TIMESTAMP(날짜+시간), JPA 2.1 까지는 @Temporal은 Date와 Calander 밖에 지원하지 않음
+    private Date created;
 
     public Long getId() {
         return id;
