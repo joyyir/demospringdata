24bf41803cf0a1968810de8ce16a157d2068c782
Jun-yeong Jang <joyyir@naver.com>
2020-01-27 11:33:05 +0900

    리포지토리

---
+++ src/main/java/pe/joyyir/PostRepository.java
@@ -0,0 +1,11 @@
+package pe.joyyir;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+    Page<Post> findByTitleContains(String title, Pageable pageable);
+}
---
+++ src/test/java/pe/joyyir/PostRepositoryTest.java
@@ -0,0 +1,58 @@
+package pe.joyyir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.test.annotation.Rollback;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+class PostRepositoryTest {
+    @Autowired
+    private PostRepository postRepository;
+
+    @Test
+    @Rollback(false) // 이걸 안하면 롤백이 되기 때문에 insert 쿼리가 안 날아가게 된다.
+    void crud() {
+        // Given
+        Post post = new Post();
+        post.setTitle("hello spring boot common");
+        assertThat(post.getId()).isNull();
+
+        // When
+        Post newPost = postRepository.save(post);
+
+        // Then
+        assertThat(newPost.getId()).isNotNull();
+
+        // When
+        List<Post> posts = postRepository.findAll();
+
+        // Then
+        assertThat(posts.size()).isEqualTo(1);
+        assertThat(posts).contains(newPost);
+
+        // When
+        Page<Post> page = postRepository.findAll(PageRequest.of(0, 10));
+
+        // Then
+        assertThat(page.getTotalElements()).isEqualTo(1);
+        assertThat(page.getNumber()).isEqualTo(0);
+        assertThat(page.getSize()).isEqualTo(10);
+        assertThat(page.getNumberOfElements()).isEqualTo(1); // 현재 페이지 개수
+
+        // When
+        page = postRepository.findByTitleContains("spring", PageRequest.of(0, 10));
+
+        // Then
+        assertThat(page.getTotalElements()).isEqualTo(1);
+        assertThat(page.getNumber()).isEqualTo(0);
+        assertThat(page.getSize()).isEqualTo(10);
+        assertThat(page.getNumberOfElements()).isEqualTo(1);
+    }
+}
